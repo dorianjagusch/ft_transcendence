fc6da0fdda1dce1c4e17a03718412e418d6cc40b
Meri Eskelinen <merituulie@hotmail.fi>
2024-04-03 10:51:27 +0300

    Add user rest api app

--- backend/backend/backend/serializers.py
+++ backend/backend/backend/serializers.py
@@ -5,8 +5,3 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ['id', 'login']
-
-		def update(self, instance, validated_data):
-			instance.login = validated_data.get('login', instance.login)
-			instance.save()
-			return instance
