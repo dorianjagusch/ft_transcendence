8c6d635d29aef208f0176a6f8e771af6d2ef9fee
Meri Eskelinen <merituulie@hotmail.fi>
2024-04-16 20:57:30 +0300

    Add password handling to the user management

--- backend/UserManagement/models.py
+++ backend/UserManagement/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-	login = models.CharField(max_length=30)
+	login = models.CharField(max_length=30, null=False, blank=False, unique=True)
+	password = models.CharField(max_length=30, null=False, blank=False)
+	# profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
 	def __str__(self):
 		return self.login
