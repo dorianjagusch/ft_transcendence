3d3d0540e8fc8a4d32043719676b525dae46a9af
djagusch <dorian.jagusch@gmail.com>
2024-04-16 07:40:23 +0300

    initial restoration of js files

--- frontend/static/js/registration.js
+++ frontend/static/js/registration.js
@@ -12,8 +12,8 @@ const base_url = "http://127.0.0.1:8000";
 
 
 document.getElementById("registration-form")?.addEventListener("submit", async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
-    const formData = new FormData(e.target); // Access the form using e.target
+    e.preventDefault();
+    const formData = new FormData(e.target);
     console.log(formData);
     try {
         const response = await fetch(`${base_url}/users/`, {
@@ -21,10 +21,8 @@ document.getElementById("registration-form")?.addEventListener("submit", async (
             body: formData
         });
         console.log(response.data);
-        // Handle the response as needed
     } catch (error) {
         console.error('Error:', error);
-        // Handle errors here
     }
 });
 
