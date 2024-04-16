6aaf3b59946ac4841fcc75cb645e01416a77a902
djagusch <dorian.jagusch@gmail.com>
2024-04-16 06:50:05 +0300

    Frontend rewrite

---
+++ frontend/static/js/Routes.js
@@ -0,0 +1,9 @@
+export const routes = {
+    "404": `/frontend/pages/404.html`,
+    "/": "./pages/landing.html",
+    "/frontend/": "./pages/landing.html",
+    "/login": "./frontend/pages/login.html",
+    "/register": "./frontend/pages/registration.html",
+    "/friends": "./frontend/pages/friends.html",
+    "/menu": "./frontend/pages/menu.html",
+};
