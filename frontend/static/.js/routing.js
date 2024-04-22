60beb0dfe79a5aee714bc4204bd857a0f5a873dd
djagusch <dorian.jagusch@gmail.com>
2024-04-22 18:22:15 +0300

    fixed paths to html pages

--- frontend/static/.js/routing.js
+++ frontend/static/.js/routing.js
@@ -10,19 +10,19 @@ const route = (event) => {
 }
 
 const routes = {
-	404: `/frontend/pages/404.html`,
+	404: `/pages/404.html`,
 	"/": "./pages/landing.html",
 	"/frontend/": "./pages/landing.html",
-	"/login": "./frontend/pages/login.html",
-	"/register": "./frontend/pages/registration.html",
-	"/friends": "./frontend/pages/friends.html",
-	"/menu": "./frontend/pages/menu.html",
+	"/login": "./pages/login.html",
+	"/register": "./pages/registration.html",
+	"/friends": "./pages/friends.html",
+	"/menu": "./pages/menu.html",
 }
 
 const handleLocation = async () => {
 	const path = window.location.pathname;
 	console.log(path);
-	const route = routes[path] || routes[404];
+	const route = routes[path] || routes["/"];
 	console.log(route);
 	const html = await fetch(route).then(data => data.text());
 	document.getElementById("content-container").innerHTML = html;
