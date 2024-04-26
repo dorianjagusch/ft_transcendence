92fb8d06d8ce197e2b4dd4fedd97c05dad7587ec
Dorian <74045849+dorianjagusch@users.noreply.github.com>
2024-04-26 12:07:26 +0300

    Update constants.js

--- frontend/static/js/constants.js
+++ frontend/static/js/constants.js
@@ -1,7 +1,6 @@
 const baseURL = "127.0.0.1:8080/";
 const userURL = `${baseURL}users/`;
-const loginURL = `${userURL}/login/`;
-const friendsURL = `${baseURL}friends/`;
+const loginURL = `${userURL}login/`;
 const leaderboardURL = `${baseURL}leaderboards/`;
 const settingsURL = `${baseURL}settings/`;
 
