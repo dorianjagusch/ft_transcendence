c47279b58b31c0afdf7c62b82e34aa900ff56682
djagusch <dorian.jagusch@gmail.com>
2024-04-26 10:20:52 +0300

    fix login url

--- frontend/static/js/constants.js
+++ frontend/static/js/constants.js
@@ -1,6 +1,6 @@
 const baseURL = "127.0.0.1:8080/";
 const userURL = `${baseURL}users/`;
-const loginURL = `${userURL}/login`;
+const loginURL = `${userURL}/login/`;
 const friendsURL = `${baseURL}friends/`;
 const leaderboardURL = `${baseURL}leaderboards/`;
 const settingsURL = `${baseURL}settings/`;
