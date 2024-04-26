5c394d991e7fc250a147906b3c085a149bab3713
djagusch <djagusch@student.hive.fi>
2024-04-26 09:40:00 +0300

    expand url constants

--- frontend/static/js/constants.js
+++ frontend/static/js/constants.js
@@ -1,3 +1,16 @@
-const baseURL = "127.0.0.1:8080/"
+const baseURL = "127.0.0.1:8080/";
+const userURL = `${baseURL}users/`;
+const loginURL = `${userURL}/login`;
+const friendsURL = `${baseURL}friends/`;
+const leaderboardURL = `${baseURL}leaderboards/`;
+const settingsURL = `${baseURL}settings/`;
 
-export { baseURL };
+
+export {
+		baseURL,
+		userURL,
+		loginURL,
+		friendsURL,
+		leaderboardURL,
+		settingsURL
+	};
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,11 +1,4 @@
 import showLandingPage from './pages/landingPage.js'
-import showFriendsPage from './pages/friendsPage.js'
-import showLoginPage from './pages/loginPage.js'
-import showRegisterPage from './pages/registerPage.js';
 
-showFriendsPage();
-// showLandingPage();
-// showLoginPage();
-
-// handleLocation();
+showLandingPage();
 
