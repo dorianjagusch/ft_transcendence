ec1745e26953188d0bafcc840e0c3bb7916e5e38
djagusch <dorian.jagusch@gmail.com>
2024-05-06 03:37:21 +0300

    minor fixes to how functions are called
    Unfortunately, due to JS weirdness, wrapping the navigateTo() function in a method in the abstract class does not work as straighforwardly for eventlisteners.
    In an attempt to keep using the functions still in a relative consitent way (using this.) the eventhadnlers HAVE TO be declared as arrow functions.
    Look into the scope of this and arrow functions, if you're interested

--- frontend/static/js/pages/registerPage.js
+++ frontend/static/js/pages/registerPage.js
@@ -4,96 +4,97 @@ import userService from "../services/UserService.js";
 import AView from "./AView.js";
 
 export default class extends AView {
+  constructor(params) {
+    super(params);
+    this.setTitle("Register");
+    this.registerHandler = this.registerHandler.bind(this);
+  }
 
-	constructor(params) {
-		super(params);
-		this.setTitle("Register");
-		this.registerHandler = this.registerHandler.bind(this);
-	}
+  registerHandler = async (e) => {
+    console.log("registerHandler");
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("current-password").value;
+    const repeatPassword = document.getElementById("password").value;
 
-	registerHandler = async (e) => {
-		console.log("registerHandler");
-		e.preventDefault();
-		const username = document.getElementById("username").value;
-		const password = document.getElementById("current-password").value;
-		const repeatPassword = document.getElementById("password").value;
+    if (username === "" || password === "" || repeatPassword === "") {
+      alert("Please enter all fields");
+      return;
+    }
 
-		if (username === "" || password === "" || repeatPassword === "") {
-			alert("Please enter all fields");
-			return;
-		}
+    if (password !== repeatPassword) {
+      alert("Passwords do not match");
+      return;
+    }
 
-		if (password !== repeatPassword) {
-			alert("Passwords do not match");
-			return;
-		}
+    const data = {
+      username: username,
+      password: password,
+    };
 
-		const data = {
-			username: username,
-			password: password,
-		};
+    userService
+      .postUser(data)
+      .then(() => {
+        alert("User created successfully. Please login.");
+        this.navigateTo("/login");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-		userService
-				.postUser(data)
-				.then(() => {
-				alert("User created successfully. Please login.");
-				this.navigateTo("/login");
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	};
+  appendEventListeners(element) {
+    const registerButton = element.querySelector(".primary-btn");
+    registerButton.addEventListener("click", this.registerHandler);
 
-	appendEventListeners(element) {
-		const registerButton = element.querySelector(".primary-sign-btn");
-		registerButton.addEventListener("click", this.registerHandler);
+    const loginButton = element.querySelector(".secondary-btn");
+    console.log(loginButton);
+    loginButton.addEventListener("click", () => {
+      this.navigateTo("/login");
+    });
+  }
 
-		const loginButton = element.querySelector(".secondary-sign-btn");
-		console.log(loginButton);
-		loginButton.addEventListener("click", () => {
-			this.navigateTo("/login");
-		});
-	};
+  createForm() {
+    const form = document.createElement("form");
 
-	createForm() {
-		const form = document.createElement("form");
+    const userNameField = InputField("text", "Username", "username");
+    const passwordField = InputField(
+      "password",
+      "Password",
+      "current-password"
+    );
+    const repeatPasswordField = InputField(
+      "password",
+      "Repeat Password",
+      "password"
+    );
 
-		const userNameField = InputField("text", "Username", "username");
-		const passwordField = InputField("password", "Password", "current-password");
-		const repeatPasswordField = InputField(
-			"password",
-			"Repeat Password",
-			"password"
-		);
+    const registerButton = document.createElement("button");
+    registerButton.classList.add("primary-btn");
+    registerButton.textContent = "Sign up";
 
-		const registerButton = document.createElement("button");
-		registerButton.classList.add("primary-sign-btn");
-		registerButton.textContent = "Sign up";
+    form.appendChild(userNameField);
+    form.appendChild(passwordField);
+    form.appendChild(repeatPasswordField);
+    form.appendChild(registerButton);
 
-		form.appendChild(userNameField);
-		form.appendChild(passwordField);
-		form.appendChild(repeatPasswordField);
-		form.appendChild(registerButton);
+    return form;
+  }
 
-		return form;
-	};
+  async getHTML() {
+    const modalContainer = Modal("register", "bg-secondary");
+    const registerModal = modalContainer.querySelector(".register");
+    const form = this.createForm();
+    registerModal.appendChild(form);
 
-	async getHTML(){
+    const loginButton = document.createElement("button");
+    loginButton.classList.add("secondary-btn");
+    loginButton.textContent = "Sign in";
+    modalContainer.appendChild(loginButton);
 
-		const modalContainer = Modal("register", "bg-secondary");
-		const registerModal = modalContainer.querySelector(".register");
-		const form = this.createForm();
-		registerModal.appendChild(form);
-
-		const loginButton = document.createElement("button");
-		loginButton.classList.add("secondary-sign-btn");
-		loginButton.textContent = "Sign in";
-		modalContainer.appendChild(loginButton);
-
-		const main = document.querySelector("main");
-		main.innerHTML = "";
-		main.appendChild(modalContainer);
-		this.appendEventListeners(main);
-	};
-
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+    main.appendChild(modalContainer);
+    this.appendEventListeners(main);
+  }
 }
