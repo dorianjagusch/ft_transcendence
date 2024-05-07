1df6f96d7c68429067c9bc86985e4dcda0bd2120
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-05-07 13:01:07 +0300

    62 modified comments to say is_online and not is_authenticated

--- backend/User/signals.py
+++ backend/User/signals.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 
-@receiver(user_logged_in)  # Update is_authenticated field when user logs in
+@receiver(user_logged_in)  # Update user's is_online field when user logs in
 def user_logged_in_handler(sender, request, user, **kwargs):
     user.is_online = True
     user.save()
 
-@receiver(user_logged_out)  # Update is_authenticated field when user logs out
+@receiver(user_logged_out)  # Update user's is_online field when user logs out
 def user_logged_out_handler(sender, request, user, **kwargs):
     user.is_online = False
     user.save()
