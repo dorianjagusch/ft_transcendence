bc5c2aea0d4ad863daa218ee32710074aa012fdb
djagusch <djagusch@student.hive.fi>
2024-05-07 12:44:17 +0300

    revert friendsPage.js and userService.js to branch 14

--- frontend/static/js/pages/friendsPage.js
+++ frontend/static/js/pages/friendsPage.js
@@ -1,89 +1,61 @@
 import { friendCard } from '../components/friendCard.js';
 import { requestCard } from '../components/requestCard.js';
-import { scrollContainer } from '../components/scrollContainer.js';
-import AView from './AView.js';
+import { scrollContainer } from '../components/scrollContainer.js'
 
-export default class extends AView {
-	constructor(params) {
-		super(params);
-		this.setTitle('Friends Page');
-	}
 
-	async getHTML() {
-		const friends = [
-			{
-				id: 1,
-				username: 'user1',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 2,
-				username: 'user2',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 3,
-				username: 'user3',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 4,
-				username: 'user4',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 5,
-				username: 'user5',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 6,
-				username: 'user6',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 7,
-				username: 'user7',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 8,
-				username: 'user8',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 9,
-				username: 'user9',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 10,
-				username: 'user10',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 11,
-				username: 'user11',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 12,
-				username: 'user12',
-				img: 'https://unsplash.it/150',
-			},
-			{
-				id: 13,
-				username: 'user13',
-				img: 'https://unsplash.it/150',
-			},
-		];
+const showFriends = async () => {
 
-		const friendScroller = scrollContainer(friends, friendCard);
-		friendScroller.classList.add('friends', 'bg-secondary');
+	const main = document.querySelector('main');
+	main.innerHTML = '';
 
-		const requestScroller = scrollContainer(friends, requestCard);
-		requestScroller.classList.add('friend-request', 'bg-secondary');
+	const friends = [
+    {
+      username: "test",
+      img: "https://unsplash.it/200/200",
+	    status: "online",
+    },
+    {
+      username: "test1",
+      img: "https://unsplash.it/200/200",
+	  status: "online",
+    },
+    {
+      username: "test2",
+      img: "https://unsplash.it/200/200",
+	    status: "online",
+    },
+    {
+      username: "test3",
+      img: "https://unsplash.it/200/200",
+	  status: "online",
+    },
+    {
+      username: "test4",
+      img: "https://unsplash.it/200/200",
+	  status: "online",
+    },
+    {
+      username: "test5",
+      img: "https://unsplash.it/200/200",
+	  status: "online",
+    },
+    {
+      username: "test6",
+      img: "https://unsplash.it/200/200",
+	  status: "online",
+    },
+  ];
+	// const friends = Call friendsAPI	to	get	friends in a json array
 
-		this.updateMain(friendScroller, requestScroller);
-	}
+  const friendScroller = scrollContainer(friends, friendCard);
+	friendScroller.classList.add('friends', 'bg-secondary');
+
+  const requestScroller = scrollContainer(friends, requestCard);
+	requestScroller.classList.add('friend-request', 'bg-secondary');
+
+
+	main.appendChild(friendScroller);
+	main.appendChild(requestScroller);
 }
+
+export default showFriends;
