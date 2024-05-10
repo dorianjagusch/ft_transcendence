774e9e6954886609b24dd0dc4c3265d0d23ff6ce
Azar <azar.sarikhani.agh@gmail.com>
2024-05-10 11:59:50 +0300

    add pong frontend

---
+++ frontend/static/js/pages/pongPage.js
@@ -0,0 +1,31 @@
+import ChatSocket from '../websocket.js';
+import { Pong } from '../components/pong.js';
+import AView from './AView.js';
+
+export default class extends AView {
+	constructor(params) {
+		super(params);
+		this.setTitle('Pong');
+	}
+
+	appendEventListeners() {
+		const chatSocket = ChatSocket();
+		document.addEventListener('keydown', (event) => {
+			const key = event.key;
+			document.getElementById('key-pressed').textContent = key;
+			chatSocket.send(
+				JSON.stringify({
+					message: key,
+				})
+			);
+		});
+	}
+
+	async getHTML() {
+
+		const pong = Pong();
+		this.updateMain(pong);
+		// document.getElementById('room-name').textContent = 'room'; //ADD ROOM NAME
+		this.appendEventListeners();
+	}
+}
--- frontend/static/js/route.js
+++ frontend/static/js/route.js
@@ -4,6 +4,7 @@ import registerPage from './pages/registerPage.js';
 import friendsPage from './pages/friendsPage.js';
 import friendPage from './pages/friendPage.js';
 import playPage from './pages/playPage.js';
+import pongPage from './pages/pongPage.js';
 import leaderBoard from './pages/leaderboardPage.js';
 import show404Page from './pages/404Page.js';
 
@@ -14,6 +15,7 @@ export default [
 	{ path: '/register', view: registerPage },
 	{ path: '/friends', view: friendsPage },
 	{ path: '/play', view: playPage },
+	{ path: '/pong', view: pongPage },
 	{ path: '/leaderboard', view: leaderBoard },
 	{ path: '/friends/:id', view: friendPage },
 ];
