59380aeed5a6c5e1ccdcda58b7b4411494db0e22
djagusch <dorian.jagusch@gmail.com>
2024-05-12 15:19:43 +0300

    Add progilePage content for non-friends view

--- frontend/static/js/pages/profilePage.js
+++ frontend/static/js/pages/profilePage.js
@@ -11,8 +11,44 @@ export default class extends AView {
 	}
 
 	async getHTML() {
-		
+		const user = {
+			id: 1,
+			username: 'Username',
+			img: '../../assets/img/default-user.png',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid! Reiciendis nobis, dolores optio eaque tempora debitis nulla vel magnam nam soluta quas doloribus sit odit eligendi architecto distinctio voluptas recusandae quos necessitatibus tenetur nisi po',
+		};
 
-		this.updateMain();
+
+		const main = document.querySelector('main');
+		main.classList.add('profile', userData.friendship);
+
+		this.setTitle(`${userData.username}'s Profile`);
+		const userName = profileTitle(userData.username);
+		const userImg = profileImg(userData.img);
+
+		const buttons = [
+			{className: 'decline-btn', textContent: 'Decline'},
+			{className: 'accept-btn', textContent: 'Accept'},
+		];
+
+		const actionBar = buttonBar(buttons);
+
+		// Create user placement element
+		const userPlacement = document.createElement('div');
+		userPlacement.classList.add('user-placement');
+
+		userData.placements.forEach((placement) => {
+			const smallPlacementCardElement = smallPlacementCard(
+				placement.game + ':',
+				placement.place.toString()
+			);
+			userPlacement.appendChild(smallPlacementCardElement);
+		});
+
+		// Create user description element
+		const userDescription = profileDescription(userData.description);
+
+		this.updateMain(userName, userImg, actionBar, userPlacement, userDescription);
 	}
 }
