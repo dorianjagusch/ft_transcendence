fa9653e9d6e445b6b0fd7fc4ea286d428ca90b36
Azar <azar.sarikhani.agh@gmail.com>
2024-06-02 13:19:37 +0300

    add updated version of game logic and game satatus that pass initial tests

--- backend/pong/game_logic.py
+++ backend/pong/game_logic.py
@@ -1,14 +1,13 @@
 # pong/game_logic.py
 from .constants import *
 import math
-from .game_status import PongStatus
 
 class PongGame:
-    def update_player_positions(pong_stat, game, key_press):
+    def update_player_positions(self, pong_stat, key_press):
         # Update player positions based on key press
         if not pong_stat.game_started and key_press in ('o', 'l'):
             # If player 2 moves, start the game
-            game.kick_start_game(pong_stat, key_press)
+            self.kick_start_game(pong_stat, key_press)
         if key_press == 'w':
             pong_stat.player_left_y -= PLAYER_MOVEMENT_UNIT 
         elif key_press == 's':
@@ -22,7 +21,7 @@ class PongGame:
         pong_stat.player_left_y = max(min(pong_stat.player_left_y, ( PLAYGROUND_HEIGHT / 2 ) - PLAYER_HEIGHT), 0)  # Bound lfet side player between 0 and (playground height / 2  - player height)
         pong_stat.player_right_y = max(min(pong_stat.player_right_y, ( PLAYGROUND_HEIGHT / 2 )  - PLAYER_HEIGHT), 0)  # Bound right side player between 0 and (playground height / 2 - player height)
     
-    def kick_start_game(pong_stat, key_press):
+    def kick_start_game(self, pong_stat, key_press):
         # Kick start the game based on right side player's movement
         if key_press == 'o':
             # right side Player moves upward, set ball angle to 120 degrees
@@ -34,18 +33,18 @@ class PongGame:
         pong_stat.game_started = True
     
     # Update ball position based on angle
-    def update_ball_position(pong_stat, game):
+    def update_ball_position(self, pong_stat):
         if pong_stat.game_started:
             pong_stat.ball_x += BALL_SPEED * math.cos(math.radians(pong_stat.ball_angle))
             pong_stat.ball_y += BALL_SPEED * math.sin(math.radians(pong_stat.ball_angle))
-            game.check_collisions(pong_stat, game)  # Check for collisions
+            self.check_collisions(pong_stat)  # Check for collisions
 
-    def check_collisions(pong_stat, game):
+    def check_collisions(self, pong_stat):
         # Check for collisions with players
         if pong_stat.ball_x <= pong_stat.player_left_x + PLAYER_WIDTH and pong_stat.player_left_y <= pong_stat.ball_y <= pong_stat.player_left_y + PLAYER_HEIGHT:
-            game.handle_player_collision(pong_stat, game, pong_stat.player_left_y)
+            self.handle_player_collision(pong_stat, pong_stat.player_left_y)
         elif pong_stat.ball_x >= pong_stat.player_right_x - BALL_WIDTH and pong_stat.player_right_y <= pong_stat.ball_y <= pong_stat.player_right_y + PLAYER_HEIGHT:
-            game.handle_player_collision(pong_stat, game, pong_stat.player_right_y)
+            self.handle_player_collision(pong_stat, pong_stat.player_right_y)
         
         # Check for collisions with top and bottom walls
         if pong_stat.ball_y <= 0 or pong_stat.ball_y >= PLAYGROUND_HEIGHT - BALL_HEIGHT:
@@ -53,19 +52,19 @@ class PongGame:
             
         # Check for collisions with vertical walls
         if pong_stat.ball_x <= 0:
-            game.handle_wall_collision(pong_stat, 'left')
+            self.handle_wall_collision(pong_stat, 'left')
         elif pong_stat.ball_x >= PLAYGROUND_WIDTH - BALL_WIDTH:
-            game.handle_wall_collision(pong_stat, 'right')
+            self.handle_wall_collision(pong_stat, 'right')
 
 
-    def handle_player_collision(pong_stat, game, player_y):
+    def handle_player_collision(self, pong_stat, player_y):
         # Calculate new angle based on collision with player
         relative_intersect_y = (pong_stat.player_left_y + PLAYER_HEIGHT / 2) - pong_stat.ball_y
         normalized_intersect_y = relative_intersect_y / (PLAYER_HEIGHT / 2)
         bounce_angle = normalized_intersect_y * (MAX_BOUNCE_ANGLE - MIN_BOUNCE_ANGLE)
         pong_stat.ball_angle = 180 - bounce_angle
 
-    def handle_wall_collision(pong_stat, wall):       
+    def handle_wall_collision(self, pong_stat, wall):       
         if wall == 'left':
             pong_stat.player_right_score += 1
         elif wall == 'right':
--- backend/pong/game_status.py
+++ backend/pong/game_status.py
@@ -6,10 +6,10 @@ from .game_logic import PongGame
 class PongStatus:
     def __init__(self):
         # Initialize game state
-        self.left_player_y = PLAYGROUND_HEIGHT // 2 
-        self.right_player_x = PLAYGROUND_WIDTH - WALL_MARGIN
-        self.left_player_x = WALL_MARGIN
-        self.right_player_y = PLAYGROUND_HEIGHT // 2
+        self.player_left_y = PLAYGROUND_HEIGHT // 2 
+        self.player_right_x = PLAYGROUND_WIDTH - WALL_MARGIN
+        self.player_left_x = WALL_MARGIN
+        self.player_right_y = PLAYGROUND_HEIGHT // 2
         self.ball_x = PLAYGROUND_WIDTH - WALL_MARGIN - PLAYER_WIDTH
         self.ball_y = PLAYGROUND_HEIGHT // 2
         self.ball_angle = 0 
@@ -17,16 +17,16 @@ class PongStatus:
         self.game_over = False
         self.winner = None
         self.loser = None
-        self.left_player_score = 0
-        self.right_player_score = 0
+        self.player_left_score = 0
+        self.player_right_score = 0
         self.game = PongGame()
 
     def update_positions(self, key_press):
-        self.game.update_player_positions(self, self.game, key_press)
-        self.game.update_ball_position(self, self.game)
+        self.game.update_player_positions(self, key_press)
+        self.game.update_ball_position(self)
 
-    def update_ball_position(self, key_press):
-        self.game.update_ball_position(self, self.game)
+    def update_ball_position(self):
+        self.game.update_ball_position(self)
 
     def get_game_state(self):
         game_state = {
