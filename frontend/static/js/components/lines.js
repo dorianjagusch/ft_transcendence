ba24771f0da367a09ff23f1d25ae3cefbf630291
djagusch <dorian.jagusch@gmail.com>
2024-06-04 12:55:47 +0200

    Beginning of cleaning up functions

--- frontend/static/js/components/lines.js
+++ frontend/static/js/components/lines.js
@@ -1,10 +1,26 @@
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.serSize (window.innerWidth, window.innerHeight);
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-CanvasCaptureMediaStreamTrack.position.set( 0, 0, 100 );
+camera.position.set(0, 0, 100);
 camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
+const material = new THREE.LineBasicMaterial({
+	color: 0x0000ff,
+	linewidth: 10,});
 
+const createPlayer = (position = 'left')
+
+const LineLeft = [];
+LineLeft.push(new THREE.Vector3(0, -10, 0));
+LineLeft.push(new THREE.Vector3(0, 10, 0));
+
+const geometry = new THREE.BufferGeometry().setFromPoints(LineLeft);
+const playerLeft = new THREE.Line(geometry, material);
+
+scene.add(playerLeft);
+renderer.render(scene, camera);
