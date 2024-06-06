3ea8bf1d73b30c4e3bdbd08c80f6a99c04dccf4a
Azar <azar.sarikhani.agh@gmail.com>
2024-06-06 15:30:23 +0300

    instatiate player with passed x

--- backend/pong/game.py
+++ backend/pong/game.py
@@ -8,10 +8,8 @@ from .game_stats import GameStats
 class PongStatus:
     def __init__(self):
         # Initialize game state
-        self.player_left = Player()
-        self.player_right = Player()
-        self.player_right.x = PLAYGROUND_WIDTH - WALL_MARGIN
-        self.player_left.x = WALL_MARGIN
+        self.player_left = Player(WALL_MARGIN)
+        self.player_right = Player(PLAYGROUND_WIDTH - WALL_MARGIN)
         self.ball = Ball()
         self.game_stats = GameStats()
         self.game = PongGame()
@@ -25,9 +23,11 @@ class PongStatus:
         elif key_press == 's':
             self.player_left.y = self.game.move_player(self.player_left.y, -PLAYER_MOVEMENT_UNIT)
         elif key_press == 'o':
-            self.player_right.y = self.game.move_player(self.player_left.y, PLAYER_MOVEMENT_UNIT)
+            self.player_right.y = self.game.move_player(self.player_right.y, PLAYER_MOVEMENT_UNIT)
         elif key_press == 'l':
-            self.player_right.y = self.game.move_player(self.player_left.y, -PLAYER_MOVEMENT_UNIT)
+            self.player_right.y = self.game.move_player(self.player_right.y, -PLAYER_MOVEMENT_UNIT)
+        self.player_right.y = self.game.check_boundery(self.player_right.y)
+        self.player_left.y = self.game.check_boundery(self.player_left.y)
         self.game.update_ball_position(self)
 
     def update_ball_position(self):
