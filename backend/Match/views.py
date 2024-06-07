f39ee90899d6c01f6041fd455eff5c79eb10b7b3
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-06-07 15:08:19 +0300

    141; changed responses

--- backend/Match/views.py
+++ backend/Match/views.py
@@ -33,7 +33,7 @@ class MatchView(APIView):
             return Response({'error': 'Something went wrong when creating the match and players'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         pong_match_url = f'ws://localhost:8080/pong/{match.id}?token={token.token}'
-        return Response({'match_url': pong_match_url}, status=status.HTTP_200_OK)
+        return Response(pong_match_url, status=status.HTTP_200_OK)
 
 
 class LaunchTestMatchView(APIView):
@@ -45,4 +45,4 @@ class LaunchTestMatchView(APIView):
 			return Response({'error': 'Something went wrong when creating the match and players'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 		
 		pong_match_url = f'ws://localhost:8080/pong/{match.id}?token={token.token}'
-		return Response({'match_url': pong_match_url}, status=status.HTTP_200_OK)
+		return Response(pong_match_url, status=status.HTTP_200_OK)
