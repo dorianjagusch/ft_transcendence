6f1cdbf5408375f1201e0ca4aa29712e321c969e
djagusch <dorian.jagusch@gmail.com>
2024-06-23 17:11:28 +0300

    Add play information

--- frontend/static/js/pong/pong.js
+++ frontend/static/js/pong/pong.js
@@ -1,7 +1,16 @@
+import elements from './setupSceneElements.js';
+
 const PongContainer = () => {
 	const pong = document.createElement('section');
 	pong.id = 'pong';
-	return pong;
+
+	const points = elements.createScoreBoard();
+	const instructions = elements.createIntructions();
+	const gameOver = elements.createGameOver();
+	pong.appendChild(points);
+	pong.appendChild(instructions);
+	pong.appendChild(gameOver);
+	return pong
 };
 
-export default PongContainer;
+export default {PongContainer};
--- frontend/static/js/pong/setupSceneElements.js
+++ frontend/static/js/pong/setupSceneElements.js
@@ -1,5 +1,3 @@
-import * as THREE from 'three';
-
 const PlayerScore = (side) => {
 	const PlayerScore = document.createElement('span');
 	PlayerScore.setAttribute('id', `${side}-score`);
@@ -24,11 +22,11 @@ const createScoreBoard = () => {
 };
 
 const playerInstructions = (player, upkey, downkey) => {
-	const instructionText = `${player} press ${upkey} to move up and ${downkey} to move down.`
+	const instructionText = `${player} press ${upkey} to move up and ${downkey} to move down.`;
 	const instruction = document.createElement('p');
 	instruction.textContent = instructionText;
 	return instruction;
-}
+};
 
 const createIntructions = () => {
 	const instructions = document.createElement('article');
@@ -36,33 +34,19 @@ const createIntructions = () => {
 	instructions.appendChild(playerInstructions('Player 1', 'w', 's'));
 	instructions.appendChild(playerInstructions('Player 2', 'o', 'l'));
 	return instructions;
-}
-
-const createPlayer = (constants, isLeft) => {
-	const {PLAYER_WIDTH, PLAYER_HEIGHT, WALL_MARGIN, PLAYGROUND_HEIGHT} = constants;
-
-	const playerGeometry = new THREE.BoxGeometry(PLAYER_WIDTH, PLAYER_HEIGHT, 0);
-	const playerMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-	const player = new THREE.Mesh(playerGeometry, playerMaterial);
-	if (isLeft) {
-		player.position.set(WALL_MARGIN, PLAYGROUND_HEIGHT / 2, 0);
-	} else {
-		const {PLAYGROUND_WIDTH} = constants;
-		player.position.set(
-			PLAYGROUND_WIDTH - WALL_MARGIN - PLAYER_WIDTH,
-			PLAYGROUND_HEIGHT / 2,
-			0
-		);
-	}
-	return player;
 };
 
-const createBall = (constants) => {
-	const {BALL_WIDTH, BALL_HEIGHT, PLAYGROUND_HEIGHT, PLAYGROUND_WIDTH} = constants;
-	const ballGeometry = new THREE.BoxGeometry(BALL_WIDTH, BALL_HEIGHT, 0);
-	const ballMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-	const ball = new THREE.Mesh(ballGeometry, ballMaterial);
-	ball.position.set(PLAYGROUND_WIDTH / 2, PLAYGROUND_HEIGHT / 2, 0);
-	return ball;
+const createGameOver = () => {
+	const gameOver = document.createElement('article');
+	gameOver.setAttribute('id', 'game-over');
+	const winner = document.createElement('p');
+	winner.setAttribute('id', 'winner');
+	const loser = document.createElement('p');
+	loser.setAttribute('id', 'loser');
+	gameOver.appendChild(winner);
+	gameOver.appendChild(loser);
+	// gameOver.style.display = 'none';
+	return gameOver;
 };
-export default {createBall, createPlayer, createScoreBoard, createIntructions};
+
+export default {createScoreBoard, createIntructions, createGameOver};
