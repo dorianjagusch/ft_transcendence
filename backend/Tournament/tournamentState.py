80ad55c0032c49655c0ad0cafa2b2f29986024c9
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-01 14:58:52 -0400

    178 changed 'in_progress' to 'in-progress'

--- backend/Tournament/tournamentState.py
+++ backend/Tournament/tournamentState.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class TournamentState(models.IntegerChoices):
 	LOBBY = 0, 'lobby'
-	IN_PROGRESS = 1, 'in_progress'
+	IN_PROGRESS = 1, 'in-progress'
 	FINISHED = 2, 'finished'
 	ABORTED = 3, 'aborted'
