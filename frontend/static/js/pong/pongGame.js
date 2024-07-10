10f49df67f7e90626ec7235efbac7537388b13ba
djagusch <dorian.jagusch@gmail.com>
2024-07-10 18:40:47 +0300

    Refactor the PongGame Class for better readability and more code reuse

--- frontend/static/js/pong/pongGame.js
+++ frontend/static/js/pong/pongGame.js
@@ -31,31 +31,118 @@ class PongGame {
 			Math.floor(constants.game.height * this.sizeFactor)
 		);
 		document.querySelector('#pong').appendChild(this.renderer.domElement);
+	}
 
-		this.camera = new THREE.PerspectiveCamera(
-			90,
-			constants.game.width / constants.game.height,
-			1,
-			500
-		);
+	setupCamera(constants) {
+		const horizontalFOV = 75;
+		const aspectRatio = constants.game.width / constants.game.height;
+		const verticalFOV =
+			2 *
+			Math.atan(Math.tan((horizontalFOV * Math.PI) / 360) / aspectRatio) *
+			(180 / Math.PI);
+
+		this.camera = new THREE.PerspectiveCamera(verticalFOV, aspectRatio, 1, 500);
+
 		this.camera.position.set(
 			constants.game.width / 2,
 			constants.game.height / 2,
-			constants.game.height / 2
+			constants.game.height / (2 * Math.tan(verticalFOV * Math.PI / 360))
 		);
+
 		this.camera.lookAt(constants.game.width / 2, constants.game.height / 2, 0);
+	}
 
+	createScene() {
 		this.scene = new THREE.Scene();
+	}
 
-		this.PlayerLeft = new Player(constants, true);
+	addPlayers(constants) {
+		this.PlayerLeft = new Player(constants, true, 0xff0000);
 		this.scene.add(this.PlayerLeft.player);
 
-		this.PlayerRight = new Player(constants, false);
+		this.PlayerRight = new Player(constants, false, 0x0000ff);
 		this.scene.add(this.PlayerRight.player);
+	}
 
+	addBall(constants) {
 		this.GameBall = new Ball(constants);
 		this.scene.add(this.GameBall.ball);
+	}
 
+	addPlane(constants) {
+		this.plane = new Plane(constants);
+		this.scene.add(this.plane.plane);
+		this.plane.plane.visible = false;
+	}
+
+	addLight(type, options) {
+		const light = new Light(type, {
+			color: options.color,
+			intensity: options.intensity,
+			position: options.position,
+		});
+
+		if (options.angle) {
+			light.angle = options.angle;
+		}
+
+		if (options.penumbra) {
+			light.penumbra = options.penumbra;
+		}
+
+		this.scene.add(light.light);
+		return light;
+	}
+
+	setupLights(constants) {
+		this.light = this.addLight(THREE.SpotLight, {
+			color: 0xffffff,
+			intensity: 750,
+			position: {
+				x: constants.game.width / 2,
+				y: constants.game.height / 2,
+				z: 30,
+			},
+			angle: Math.PI / 64,
+		});
+
+		this.light2 = this.addLight(THREE.SpotLight, {
+			color: this.PlayerLeft.color,
+			intensity: 1000,
+			position: {
+				x: this.PlayerRight.player.position.x,
+				y: this.PlayerRight.player.position.y,
+				z: 30,
+			},
+			penumbra: 0.7,
+		});
+
+		this.light3 = this.addLight(THREE.SpotLight, {
+			color: this.PlayerRight.color,
+			intensity: 1000,
+			position: {
+				x: this.PlayerLeft.player.position.x,
+				y: this.PlayerLeft.player.position.y,
+				z: 50,
+			},
+			penumbra: 0.7,
+		});
+
+		this.AmbientLight = this.addLight(THREE.AmbientLight, {
+			color: 0x404040,
+			intensity: 3,
+			position: {},
+		});
+	}
+
+	setupGame(constants) {
+		this.initializeRenderer(constants);
+		this.setupCamera(constants);
+		this.createScene();
+		this.addPlayers(constants);
+		this.addBall(constants);
+		this.addPlane(constants);
+		this.setupLights(constants);
 		this.renderer.render(this.scene, this.camera);
 	}
 
@@ -86,6 +173,15 @@ class PongGame {
 		this.PlayerLeft.player.position.setY(players.left.position.y);
 		this.PlayerRight.player.position.setY(players.right.position.y);
 		this.GameBall.ball.position.set(ball.position.x, ball.position.y, 0);
+
+		if (ball.depth){
+			this.GameBall.ball.rotation.x += 0.1;
+			const rotationSpeed = 0.075;
+			this.GameBall.ball.rotation.y += rotationSpeed;
+		}
+		this.light.followObject(this.GameBall.ball);
+		this.light2.followObject(this.PlayerLeft.player);
+		this.light3.followObject(this.PlayerRight.player);
 		this.updateScore(players);
 		this.renderer.render(this.scene, this.camera);
 	}
