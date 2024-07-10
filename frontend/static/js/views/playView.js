fca86d85d0765ea5d3b3a448a01cdff763e8231a
djagusch <dorian.jagusch@gmail.com>
2024-07-10 08:52:42 +0300

    Add matchView

--- frontend/static/js/views/playView.js
+++ frontend/static/js/views/playView.js
@@ -7,11 +7,18 @@ export default class extends AView {
 		this.setTitle('Play');
 	}
 
+	attachEventListeners() {
+		const gameCard = document.querySelector('.game-card');
+		gameCard.addEventListener('click', () => {
+			this.navigateTo('/match');
+		});
+	}
+
 	async getHTML() {
 		const gameOne = GameCard('Pong', 'pong-front', 'pong-card');
-
 		this.updateMain(gameOne);
 		const main = document.querySelector('main');
 		main.classList.add('flex-row');
+		this.attachEventListeners();
 	}
 }
