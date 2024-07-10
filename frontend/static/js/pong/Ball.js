48e3fe2a9517845caa4fde94e601b9954e1e1fb1
djagusch <dorian.jagusch@gmail.com>
2024-07-10 18:34:09 +0300

    give ball helper functions for dimension switch

--- frontend/static/js/pong/Ball.js
+++ frontend/static/js/pong/Ball.js
@@ -2,15 +2,43 @@ import * as THREE from 'three';
 
 class Ball {
 	constructor(constants) {
+		this.materials = {
+			default: new THREE.MeshBasicMaterial({color: 0xffffff}),
+			alternative: new THREE.MeshLambertMaterial({color: 0xffffff}),
+		};
+		this.size = constants.ball.size;
+		this.depth = 0;
 		this.ball = this.createBall(constants);
 	}
 
-	createBall({ball: {size}, game}) {
-		const ballGeometry = new THREE.BoxGeometry(size, size, 0);
-		const ballMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+	createBall({game}) {
+		const ballGeometry = new THREE.BoxGeometry(this.size, this.size, this.depth);
+		const ballMaterial = this.materials.default;
 		const ball = new THREE.Mesh(ballGeometry, ballMaterial);
 		ball.position.set(Math.floor(game.width / 2), Math.floor(game.height / 2), 0);
 		return ball;
+	}
+
+	switchMaterial() {
+		if (this.ball.material === this.materials.default) {
+			this.ball.material = this.materials.alternative;
+			this.ball.castShadow = true;
+			this.ball.receiveShadow = true;
+		} else {
+			this.ball.material = this.materials.default;
+			this.ball.castShadow = false;
+			this.ball.receiveShadow = false;
+		}
+	}
+
+	updateBallDepth() {
+		const ballGeometry = new THREE.BoxGeometry(
+			this.size,
+			this.size,
+			this.depth ? 0 : this.size
+		);
+		this.ball.geometry.dispose();
+		this.ball.geometry = ballGeometry;
 	}
 }
 export default Ball;
