a74d6a1861d542cf289bffb9cada6eca34c6752b
djagusch <dorian.jagusch@gmail.com>
2024-07-11 20:08:10 +0300

    add not ImplementedError to Services

--- frontend/static/js/services/logoutService.js
+++ frontend/static/js/services/logoutService.js
@@ -9,6 +9,22 @@ class LogoutService extends ARequestService {
 	async postRequest() {
 		return super.postRequest(backendURL.logoutURL);
 	}
+
+	getAllRequest() {
+		throw new NotImplementedError('GET (all) not implemented for LogoutService');
+	}
+
+	getRequest() {
+		throw new NotImplementedError('GET method not implemented for LogoutService');
+	}
+
+	putRequest() {
+		throw new NotImplementedError('PUT method not implemented for LogoutService');
+	}
+
+	deleteRequest() {
+		throw new NotImplementedError('DELETE method not implemented for LogoutService');
+	}
 }
 
 export default LogoutService;
--- frontend/static/js/services/pongService.js
+++ frontend/static/js/services/pongService.js
@@ -1,5 +1,6 @@
 import ARequestService from './ARequestService.js';
 import backendURL from '../constants.js';
+import NotImplementedError from '../exceptions/NotImplementedException.js';
 
 class PongService extends ARequestService {
 	constructor() {
@@ -9,6 +10,24 @@ class PongService extends ARequestService {
 	async getRequest() {
 		return super.getRequest(backendURL.pongURL);
 	}
+
+	getAllRequest() {
+		new NotImplementedError('GET (all) not implemented for PongService');
+
+	}
+
+	postRequest(data){
+		new NotImplementedError('POST method not implemented for PongService');
+	}
+
+	putRequest(data){
+		new NotImplementedError('PUT method not implemented for PongService');
+	}
+
+	deleteRequest(){
+		new NotImplementedError('DELETE method not implemented for PongService');
+	}
+
 }
 
 export default PongService;
