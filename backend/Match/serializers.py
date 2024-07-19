6526ce5217ad5c80b2bd88c41bae2b88ce6c9f96
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-19 14:11:46 +0300

    changed MatchSerializer to only have state str

--- backend/Match/serializers.py
+++ backend/Match/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Match
 
 class MatchSerializer(serializers.ModelSerializer):
-	state_display = serializers.CharField(source='get_state_display', read_only=True)
+	state = serializers.CharField(source='get_state_display', read_only=True)
 
 	class Meta:
 		model = Match
-		fields = ['id', 'state', 'state_display', 'tournament', 'start_time', 'end_time', 'created_at', 'updated_at']
+		fields = ['id', 'state', 'tournament', 'start_time', 'end_time', 'created_at', 'updated_at']
