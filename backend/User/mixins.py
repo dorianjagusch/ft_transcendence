7a9a7ed4f705d4a9290eb9d1e54ad21bcaa376f4
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-21 14:10:06 +0300

    create User/mixins.py file; created UserAuthenticationMixin

---
+++ backend/User/mixins.py
@@ -0,0 +1,22 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+
+from .models import User
+
+class UserAuthenticationMixin:
+	def authenticate_user(self, request: Request) -> User | Response:
+		username = request.data.get('username', None)
+		password = request.data.get('password', None)
+
+		if not username or not password:
+			return Response({"message": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+		
+		user = authenticate(username, password)
+		if user is not None:
+			return user
+		else:
+			return Response({"message": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+
+		
