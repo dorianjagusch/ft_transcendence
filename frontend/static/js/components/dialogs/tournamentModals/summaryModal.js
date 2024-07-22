fd6b2b7b22a3cf0f27600569eee590a6c47cf2c6
djagusch <dorian.jagusch@gmail.com>
2024-07-22 16:40:20 +0300

    Clean up summary modal

--- frontend/static/js/components/dialogs/tournamentModals/summaryModal.js
+++ frontend/static/js/components/dialogs/tournamentModals/summaryModal.js
@@ -6,23 +6,6 @@ export default class SummaryModal extends ADialog {
 	constructor(parentDataHandler, tournamentData) {
 		super(new SummaryForm(tournamentData), new TournamentService());
 		this.tournamentData = tournamentData;
-	}
-
-	appendEventlistenters() {
-		this.dialog.addEventListener(
-			'click',
-			async (e) => {
-				if (e.target.classList.contains('accept-btn')) {
-					e.preventDefault();
-					try {
-						const TournamentData = await this.service.getRequest(this.tournamentData.id);
-						this.onDataReceived(TournamentData);
-					} catch (error) {
-						this.notify(error.message);
-					}
-				}
-			},
-			false
-		);
+		this.onDataReceived = parentDataHandler;
 	}
 }
