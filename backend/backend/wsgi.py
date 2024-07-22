ae62c8f57c0b6e7004f87cc78a2b7586e6121182
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-22 15:14:31 +0300

    formatted wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,16 +1,17 @@
 """
 WSGI config for backend project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named `application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'django' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+# Get the WSGI application for Django.
 application = get_wsgi_application()
