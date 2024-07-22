f775dd3a7f95f1c9c40e9dc63312a4eed05494b7
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-22 11:27:31 +0300

    format validators.py

--- backend/User/validators.py
+++ backend/User/validators.py
@@ -2,16 +2,32 @@ from django.core.exceptions import ValidationError
 import os
 
 def validate_image(file):
-	valid_mime_types = ['image/jpeg', 'image/png', 'image/gif']
-	valid_file_extensions = ['.jpg', '.jpeg', '.png', '.gif']
-	max_file_size = 2097152
+    """
+    Validate the uploaded image file.
+    
+    Parameters:
+    file (InMemoryUploadedFile): The uploaded file.
 
-	ext = os.path.splitext(file.name)[1].lower()
-	if ext not in valid_file_extensions:
-		raise ValidationError('Unsupported file extension. Allowed extensions are: .jpg, .jpeg, .png, .gif.')
+    Raises:
+    ValidationError: If the file does not meet the validation criteria.
+    """
+    valid_mime_types = ['image/jpeg', 'image/png', 'image/gif']
+    valid_file_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    max_file_size = 2097152  # 2MB
 
-	if file.size > max_file_size:
-		raise ValidationError('File size exceeds the maximum limit of 2MB.')
+    ext = os.path.splitext(file.name)[1].lower()
+    if ext not in valid_file_extensions:
+        raise ValidationError(
+            'Unsupported file extension. Allowed extensions are: .jpg, .jpeg, .png, .gif.'
+        )
 
-	if file.content_type not in valid_mime_types:
-		raise ValidationError('Unsupported file type. Allowed types are: jpeg, png, gif.')
+    if file.size > max_file_size:
+        raise ValidationError(
+            'File size exceeds the maximum limit of 2MB.'
+        )
+
+    if file.content_type not in valid_mime_types:
+        raise ValidationError(
+            'Unsupported file type. Allowed types are: jpeg, png, gif.'
+        )
+
