e5dabf0ba9578b21a51d2c747882cf8374460990
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-22 11:30:35 +0300

    format signals.py

--- backend/User/signals.py
+++ backend/User/signals.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 
-@receiver(user_logged_in)  # Update user's is_online field when user logs in
+@receiver(user_logged_in)
 def user_logged_in_handler(sender, request, user, **kwargs):
+    """
+    Update user's is_online field when user logs in.
+    """
     user.is_online = True
     user.save()
 
-@receiver(user_logged_out)  # Update user's is_online field when user logs out
+@receiver(user_logged_out)
 def user_logged_out_handler(sender, request, user, **kwargs):
+    """
+    Update user's is_online field when user logs out.
+    """
     user.is_online = False
     user.save()
