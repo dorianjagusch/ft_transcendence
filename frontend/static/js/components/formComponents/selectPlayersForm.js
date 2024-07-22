0a8b3bc588e578dad00bedb3e2ff5375951fa3e6
djagusch <dorian.jagusch@gmail.com>
2024-07-22 16:41:44 +0300

    refactor Playercard and associated forms to use the new API

--- frontend/static/js/components/formComponents/selectPlayersForm.js
+++ frontend/static/js/components/formComponents/selectPlayersForm.js
@@ -1,35 +1,43 @@
 import getProfilePicture from '../profilePicture.js';
 import AForm from './AForm.js';
-import PlayerCard from './PlayerCard.js';
+import {PlayerCard} from './PlayerCard.js';
 
 export default class selectPlayersForm extends AForm {
 	constructor({tournament, tournament_player}) {
 		super();
 		this.numberOfPlayers = tournament.player_amount;
 		this.tournamentName = tournament.name;
-		this.tournamenHostId = tournament_player.id;
+		this.tournamentHostId = tournament_player.id;
 		this.playerIds = [];
 		this.generateForm = this.generateForm.bind(this);
 	}
 
-	generateForm() {
-		const form = document.createElement('form');
-		form.classList.add('player-selection');
+	createFormButton(type, text) {
+		const Button = document.createElement('button');
+		Button.classList.add(type);
+		Button.textContent = text;
+		Button.setAttribute('type', 'submit');
+		Button.setAttribute('formmethod', 'dialog');
+		return Button;
+	}
 
-		const playerList = document.createElement('section');
-		playerList.classList.add('player-list', `player-${this.numberOfPlayers}`);
-		form.appendChild(playerList);
-		debugger;
+	createPlayerCards(playerContainer) {
 		for (let i = 0; i < this.numberOfPlayers; i++) {
 			if (i == 0) {
 				const playerCard = PlayerCard(
 					localStorage.getItem('username'),
-					getProfilePicture(localStorage.getItem('user_id')),
 					this.tournamentHostId,
 					true
 				);
-				playerCard.setAttribute('data-user-selected', 'true')
-				playerList.appendChild(playerCard);
+				playerCard.setAttribute('data-user-selected', 'true');
+				playerCard.setAttribute('data-player-id', this.tournamentHostId);
+				playerContainer.appendChild(playerCard);
+				getProfilePicture(localStorage.getItem('user_id'))
+				.then((img) => {
+					playerCard.querySelector(
+						`[data-player-id="${this.tournamentHostId}"] > .player-img`
+					).src = img;
+				});
 				continue;
 			}
 			const playerCard = PlayerCard(
@@ -39,27 +47,27 @@ export default class selectPlayersForm extends AForm {
 				true
 			);
 			playerCard.classList.add('bg-inactive');
-			playerList.appendChild(playerCard);
+			playerContainer.appendChild(playerCard);
 		}
+	}
+
+	generateForm() {
+		this.form.classList.add('player-selection');
+
+		const playerList = document.createElement('section');
+		playerList.classList.add('player-list', `player-${this.numberOfPlayers}`);
+
+		this.createPlayerCards(playerList);
 
 		const buttonBar = document.createElement('div');
 		buttonBar.classList.add('button-bar', 'flex-row');
-		form.appendChild(buttonBar);
 
-		const backButton = document.createElement('button');
-		backButton.classList.add('decline-btn');
-		backButton.textContent = 'Back';
-		backButton.setAttribute('type', 'submit');
-		backButton.setAttribute('formmethod', 'dialog');
+		const backButton = this.createFormButton('decline-btn', 'Back');
+		const continueButton = this.createFormButton('accept-btn', 'Continue');
 		buttonBar.appendChild(backButton);
-
-		const continueButton = document.createElement('button');
-		continueButton.classList.add('accept-btn');
-		continueButton.textContent = 'Continue';
-		continueButton.setAttribute('type', 'submit');
-		continueButton.setAttribute('formmethod', 'dialog');
 		buttonBar.appendChild(continueButton);
 
-		return form;
+		this.appendToForm(playerList, buttonBar);
+		return this.getForm();
 	}
 }
