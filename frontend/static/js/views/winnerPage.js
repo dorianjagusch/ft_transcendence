9213937bcf5c21f9927f6a35ae38706ff4ba4b79
djagusch <dorian.jagusch@gmail.com>
2024-07-25 22:56:35 +0300

    leave instructions for next step

--- frontend/static/js/views/winnerPage.js
+++ frontend/static/js/views/winnerPage.js
@@ -1,31 +1,30 @@
 import AView from './AView.js';
 import {PlayerInfo} from '../components/playerInfo.js';
 import TournamentService from '../services/tournamentService.js';
-import MatchService from '../services/matchService.js';
-import PlayerService from '../services/playerService.js';
+// import MatchService from '../services/matchService.js';
+// import PlayerService from '../services/playerService.js';
 
 export default class extends AView {
 	constructor(params) {
 		super(params);
 		this.setTitle('Tournament Winner');
 		this.tournamentService = new TournamentService();
-		this.matchService = new MatchService();
-		this.PlayerService = new PlayerService();
+		// this.matchService = new MatchService();
+		// this.PlayerService = new PlayerService();
 	}
 
 	async getHTML() {
 		if (this.params.tournament_id) {
 			try {
 				const tournamentData = await this.tournamentService.getTournamentMatches(this.context);
-				const winner = tournamentData[tournamentData.length - 1].winner;
-				debugger
+				const winner = tournamentData[tournamentData.length - 1];
+				debugger // TODO: Need to implement getting the actual user id instead of the current player id
 				winner.img = await getProfilePicture(winner.id);
-				console.log(winner);
 			} catch (error) {
 				this.notify(error.message);
 			}
 		} else {
-			this.matchService()
+			console.log ("Implement individual match handling") // TODO: Implement individual match handling
 		}
 		const congratsElement = document.createElement('h2');
 		congratsElement.textContent = 'Congratulations!';
