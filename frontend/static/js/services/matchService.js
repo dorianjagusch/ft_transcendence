5133e491b2fb7715d1275980d8865e66249c115c
djagusch <dorian.jagusch@gmail.com>
2024-07-25 22:00:24 +0300

    add matchService

---
+++ frontend/static/js/services/matchService.js
@@ -0,0 +1,12 @@
+import ARequestService from './ARequestService.js';
+import backendURL from '../constants.js';
+
+export default class extends ARequestService {
+	contructor() {
+		super();
+	}
+
+	async getUserMatches(matchId, user_id) {
+		return await this.get(`matches/${matchId}/${user_id}`);
+	}
+}
