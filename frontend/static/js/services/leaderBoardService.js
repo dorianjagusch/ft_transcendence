3273ce28ce16f14858997cf323d23894d921b92a
djagusch <dorian.jagusch@gmail.com>
2024-07-25 23:06:05 +0300

    Add leaderboard service

--- frontend/static/js/services/leaderBoardService.js
+++ frontend/static/js/services/leaderBoardService.js
@@ -1,12 +1,13 @@
+import ARequestService from "./ARequestService.js";
 
-const getAll = async () => {
-	const url = 'http://127.0.0.1:8080/leaderboard';
-	const response = await fetch(url);
-	if (response.status !== 200) {
-		throw new Error('Failed to fetch leaderboard');
+export default class LeaderBoardService extends ARequestService {
+
+	constructor() {
+		super();
 	}
-	const data = await response.json();
-	return data;
-}
 
-export default {getAll};
+	async getLeaderBoard() {
+		return super.getRequest('leaderboard/');
+	}
+
+}
