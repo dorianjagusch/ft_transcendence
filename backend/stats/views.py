32d1350f500165f7376aaae00047fa6c88abfe86
Azar <azar.sarikhani.agh@gmail.com>
2024-07-25 09:41:14 +0300

    add win loss pie chart graph

--- backend/stats/views.py
+++ backend/stats/views.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from django.utils.decorators import method_decorator
 from rest_framework.response import Response
+from django.http import HttpResponse
 from rest_framework import status
 
 from .user_stats_table import UserTable
@@ -36,3 +37,32 @@ class StatsView(APIView, UserTableMixin):
 
 		return Response(data, status=status.HTTP_200_OK)
 
+class StatsGraphView(APIView, UserTableMixin):
+	@method_decorator(must_be_authenticated)
+	def get (self, request, user_id):
+		user = User.objects.filter(id = user_id).first()
+		if not user:
+			return Response({"message": "User was not found"}, status=status.HTTP_404_NOT_FOUND)
+		wins = self.get_wins_count(user)
+		losses = self.get_losses_count(user)
+		total_games_played = self.get_total_games_played_count(user)
+		# If no games are played, the whole circle will be gray
+		if total_games_played == 0:
+			labels = ['No Games Played']
+			values = [1]
+			colors = ['gray']
+		else:
+			labels = ['Wins', 'Losses']
+			values = [wins, losses]
+			colors = ['green', 'red']
+		
+		returnfig = go.Figure(data=[go.Pie(
+            labels=labels,
+            values=values,
+            marker=dict(colors=colors),
+            hole=0.3  # This is optional, it creates a donut chart if > 0
+        )])
+		# Convert the Plotly figure to an SVG string
+		svg_str = fig.to_image(format="svg").decode("utf-8")
+
+		return HttpResponse(svg_str, content_type='image/svg+xml')
