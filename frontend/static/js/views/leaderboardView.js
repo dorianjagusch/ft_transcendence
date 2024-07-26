188bc46722afd0cec41c4b2548fdb2fc08e67951
djagusch <dorian.jagusch@gmail.com>
2024-07-26 15:49:48 +0300

    Add API data to frontend

--- frontend/static/js/views/leaderboardView.js
+++ frontend/static/js/views/leaderboardView.js
@@ -1,124 +1,74 @@
-import { scrollContainer } from '../components/scrollContainer.js';
-import { PlacementCard } from '../components/placementCard.js';
+import {scrollContainer} from '../components/scrollContainer.js';
+import {PlacementCard} from '../components/placementCard.js';
 import AView from './AView.js';
 import LeaderBoardService from '../services/leaderBoardService.js';
-
+import UserService from '../services/userService.js';
+import getProfilePicture from '../components/profilePicture.js';
 
 export default class extends AView {
 	constructor(params) {
 		super(params);
 		this.setTitle('Leaderboard');
 		this.leaderBoardService = new LeaderBoardService();
+		this.userService = new UserService();
+		this.appendEventListeners = this.appendEventListeners.bind(this);
+	}
+
+	async getPlayers() {
+		try {
+			const playerData = await this.leaderBoardService.getLeaderBoard();
+			const players = [];
+
+			for (const [index, player] of playerData.entries()) {
+				if (player.wins == 0) {
+					break;
+				}
+				const img = await getProfilePicture(player.user);
+				const userData = await this.userService.getRequest(player.user);
+				const username = userData.username;
+				players.push({
+					username: username,
+					userId: player.user,
+					wins: player.wins,
+					img: img,
+					place: index + 1,
+				});
+			}
+
+			return players;
+		} catch (error) {
+			this.notify(error, 'error');
+			return null;
+		}
+	}
+
+	appendEventListeners() {
+		document.querySelector('.col-scroll').addEventListener('click', (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			console.log('clicked button');
+			const card = e.target.closest('.placement-card');
+			if (!card) {
+				return;
+			}
+			const userId = card.getAttribute('data-user-id');
+			this.navigateTo(`/profile/${userId}`);
+		});
 	}
 
 	async getHTML() {
-		// const players = [
-		// 	{
-		// 		username: 'player1',
-		// 		wins: 1000,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 1,
-		// 	},
-		// 	{
-		// 		username: 'player2',
-		// 		wins: 800,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 2,
-		// 	},
-		// 	{
-		// 		username: 'player3',
-		// 		wins: 700,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 3,
-		// 	},
-		// 	{
-		// 		username: 'player4',
-		// 		wins: 600,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 4,
-		// 	},
-		// 	{
-		// 		username: 'player5',
-		// 		wins: 500,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 5,
-		// 	},
-		// 	{
-		// 		username: 'player6',
-		// 		wins: 400,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 6,
-		// 	},
-		// 	{
-		// 		username: 'player7',
-		// 		wins: 300,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 7,
-		// 	},
-		// 	{
-		// 		username: 'player8',
-		// 		wins: 200,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 8,
-		// 	},
-		// 	{
-		// 		username: 'player9',
-		// 		wins: 100,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 9,
-		// 	},
-		// 	{
-		// 		username: 'player10',
-		// 		wins: 90,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 10,
-		// 	},
-		// 	{
-		// 		username: 'player11',
-		// 		wins: 80,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 11,
-		// 	},
-		// 	{
-		// 		username: 'player12',
-		// 		wins: 70,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 12,
-		// 	},
-		// 	{
-		// 		username: 'player13',
-		// 		wins: 60,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 13,
-		// 	},
-		// 	{
-		// 		username: 'player14',
-		// 		wins: 50,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 14,
-		// 	},
-		// 	{
-		// 		username: 'player15',
-		// 		wins: 40,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 15,
-		// 	},
-		// 	{
-		// 		username: 'player16',
-		// 		wins: 30,
-		// 		img: 'http://unsplash.it/200',
-		// 		place: 16,
-		// 	},
-		// ];
-
-		// get players from API
-		debugger;
-		const players = await this.leaderBoardService.getLeaderBoard();
-
-		const leaderBoardOne = scrollContainer(players, PlacementCard, 'column');
+		const players = await this.getPlayers();
+		if (!players) {
+			return 'No data yet';
+		}
+		const leaderBoardOne = scrollContainer(players, PlacementCard, 'col');
 		leaderBoardOne.classList.add('leaderboard', 'bg-secondary');
+		const header = document.createElement('h2');
+		header.textContent = 'Leaderboard';
+		leaderBoardOne.insertBefore(header, leaderBoardOne.firstChild);
+		const innerScroller = leaderBoardOne.querySelector('.col-scroll');
+		innerScroller.style.gridAutoRows = '5em';
 		this.updateMain(leaderBoardOne);
-
-
+		this.appendEventListeners();
 	}
 }
