89dca32eaaac78dc4e1632f45dfb08ee5c8d9af8
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-29 13:14:08 +0300

    added running of daphne to ssl_setup.sh

--- backend/ssl_setup.sh
+++ backend/ssl_setup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+SSL_KEY_PATH="/app/ssl/certificate.key"
+SSL_CERT_PATH="/app/ssl/certificate.crt"
+
 mkdir -p /etc/ssl
 
 openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes \
-	-out /etc/ssl/certificate.crt \
-	-keyout /etc/ssl/certificate.key \
+	-out  ${SSL_CERT_PATH}\
+	-keyout ${SSL_KEY_PATH} \
 	-subj "/C=FI/ST=Helsinki/L=Helsinki/O=Hive/OU=transcendence/CN=transcendence.42.fi"
+
+daphne -e ssl:443:privateKey=${SSL_KEY_PATH}:certKey=${SSL_CERT_PATH} backend.asgi:application
