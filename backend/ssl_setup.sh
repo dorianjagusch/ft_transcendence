b21629e80c540f3c44d3e9a87a2109821b645aa9
sakarisalmi <sakarisalmi.personal@gmail.com>
2024-07-29 13:08:53 +0300

    changed directory for certificate crt and key

--- backend/ssl_setup.sh
+++ backend/ssl_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p /etc/nginx/ssl
+mkdir -p /etc/ssl
 
 openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes \
-	-out /etc/nginx/ssl/certificate.crt \
-	-keyout /etc/nginx/ssl/certificate.key \
+	-out /etc/ssl/certificate.crt \
+	-keyout /etc/ssl/certificate.key \
 	-subj "/C=FI/ST=Helsinki/L=Helsinki/O=Hive/OU=transcendence/CN=transcendence.42.fi"
