4da1ed233eba6f9622d7f6096162344ad75ca1cc
djagusch <dorian.jagusch@gmail.com>
2024-07-30 19:54:59 +0300

    Replace parseInt() with Number() in pongView

--- frontend/static/js/views/pongView.js
+++ frontend/static/js/views/pongView.js
@@ -70,7 +70,8 @@ export default class extends AView {
 				this.navigateTo(`/winner/match/${parseWStoMatchId(this.matchUrl)}`);
 				return;
 			}
-			const match_id = parseInt(this.params.match_id) + 1;
+			const match_id = Number(this.params.match_id) + 1;
+			debugger
 			if (await this.isTournamentFinished(this.params)) {
 				this.navigateTo(`/winner/tournaments/${this.params.tournament_id}`);
 			} else {
